7b7295ec59ee09f83afc5471b51ea96df8a001bf
kinetiqsport <kinetiqsport@users.noreply.github.com>
2026-01-18 02:00:15 +0000

    deploy: d88233e9e5c2ff4c63044a200c5bfbb7e06ab46e

--- google-auth-config.js
+++
@@ -1,4 +0,0 @@
-// Configuración de Google OAuth
-// Inyectada automáticamente por GitHub Actions
-window.GOOGLE_CLIENT_ID = '1042371491410-bkkfij37r2csku7cl6baaaca7vdd94sn.apps.googleusercontent.com';
-console.log('Google Auth Config loaded');
